5e2bd5a7da64fa6058e37c84b2ab7570474582c5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-18 16:39:25 +0200

    Clone repo once

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -7,4 +7,3 @@ git clone https://github.com/richelbilderbeek/Newick
 git clone https://github.com/richelbilderbeek/NewickVector
 git clone https://github.com/richelbilderbeek/RibiClasses
 git clone https://github.com/richelbilderbeek/RibiLibraries
-git clone https://github.com/richelbilderbeek/BinaryNewickVector
